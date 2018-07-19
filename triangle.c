8a1eafade7a615a9b34e9472967617b1ae1098fa
Sonalkumar Kadelwar <sonalkumarkadelwar@gmail.com>
2018-07-19 15:07:24 +0530

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+  int i,j,n=5;
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<i+n;j++)
+    {
+      if(i==n-1)
+      {
+        printf("* ");
+        j++;
+      }
+      else
+      {
+          if(i+j==n-1 || (i+j)-2*i==n-1 )
+       {
+        printf("*");
+       }
+       else
+        printf(" ");
+        }
+      
+    }
+    printf("\n");
+  }
+  return 0;
+}
