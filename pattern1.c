138a2d27d1eadbfeed4fed6f38a8820adf14a2e3
Sonalkumar Kadelwar <sonalkumarkadelwar@gmail.com>
2018-07-20 19:40:51 +0530

    Create pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,24 @@
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  int i,j,n=5;
+  for (i=0;i<n;i++)
+  {
+    for(j=0;j<=i;j++)
+    {
+      printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
