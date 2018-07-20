99e3a06fdfe3c06a97309765ce64b4dcd5672f60
Sonalkumar Kadelwar <sonalkumarkadelwar@gmail.com>
2018-07-20 20:00:27 +0530

    Create filled_triangle.c

---
+++ filled_triangle.c
@@ -0,0 +1,32 @@
+/*
+
+    *
+   ***
+  *****
+ *******
+*********
+
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  int i,j,n=5;
+  for(i=0;i<n;i++)
+  {
+      for(j=0;j<i+n;j++)
+      {
+        if(i+j<n-1)
+        {
+          printf(" ");
+        }
+        else 
+          printf("*");
+      }
+      
+    
+    printf("\n");
+  }
+  return 0;
+}
